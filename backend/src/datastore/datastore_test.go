d1114b3d5ac4fd2dc0e71461dcb161df35814bfa
abrie <abrie@users.noreply.github.com>
2021-12-05 07:48:06 -0500

    Add Handler test.

--- backend/src/datastore/datastore_test.go
+++ backend/src/datastore/datastore_test.go
@@ -3,10 +3,57 @@ package datastore
 import (
 	"backend/db"
 	"backend/models/memory"
+	"bytes"
+	"encoding/json"
 	"log"
+	"net/http"
+	"net/http/httptest"
 	"reflect"
 	"testing"
 )
+
+func TestMemoryHandler(t *testing.T) {
+	db, err := db.Open(":memory:")
+	if err != nil {
+		t.Fatalf("Failed to open database: %v", err)
+	}
+
+	datastore, err := Open(db)
+	if err != nil {
+		t.Fatalf("Failed to create Datastore: %v", err)
+	}
+
+	memories := []memory.Memory{
+		{Text: "memory1", Timestamp: 1, Latitude: 10.5, Longitude: 9.5},
+		{Text: "memory2", Timestamp: 2, Latitude: 20.5, Longitude: 19.5},
+	}
+
+	w := httptest.NewRecorder()
+
+	for idx := range memories {
+		buf := new(bytes.Buffer)
+		json.NewEncoder(buf).Encode(memories[idx])
+		r := httptest.NewRequest(http.MethodPost, "/", buf)
+
+		datastore.MemoryHandler(w, r)
+	}
+
+	got, err := datastore.GetMemories()
+	if err != nil {
+		t.Fatalf("Failed to get memories: %v", err)
+	}
+
+	if len(got) != len(memories) {
+		t.Fatalf("Expected %d memories, got: %d", len(memories), len(got))
+	}
+
+	for idx := range memories {
+		if same := reflect.DeepEqual(memories[idx], got[idx]); !same {
+			log.Fatalf("Expected %v, Got: %v", memories[idx], got[idx])
+		}
+	}
+
+}
 
 func TestPutMemory(t *testing.T) {
 	db, err := db.Open(":memory:")
